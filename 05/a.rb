ff02669b0438b2691602c80d1cd9a949cee3a9ea
Joey Schoblaska <joey.schoblaska@gusto.com>
2022-12-05 10:04:44 -0600

    use IO::foreach instead of ::readlines .. #each

--- 05/a.rb
+++ 05/a.rb
@@ -2,15 +2,13 @@ require_relative "../lib/scan_with_index.rb"
 
 stacks = Hash.new { |h, k| h[k] = [] }
 
-File
-  .readlines("05/input.txt", chomp: true)
-  .each do |line|
-    if line =~ /\[[A-Z]\]/
-      line.scan_with_index(/[A-Z]/) { |m, i| stacks[i / 4 + 1] << m }
-    elsif line =~ /move/
-      count, from, to = line.scan(/\d+/).map(&:to_i)
-      count.times { stacks[to].unshift(stacks[from].shift) }
-    end
+File.foreach("05/input.txt", chomp: true) do |line|
+  if line =~ /\[[A-Z]\]/
+    line.scan_with_index(/[A-Z]/) { |m, i| stacks[i / 4 + 1] << m }
+  elsif line =~ /move/
+    count, from, to = line.scan(/\d+/).map(&:to_i)
+    count.times { stacks[to].unshift(stacks[from].shift) }
   end
+end
 
 puts stacks.keys.sort.map { |k| stacks[k][0] }.join
