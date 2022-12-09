44435c4cd1c4b20372fa6e73dd9a59ed6530b085
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-12-09 09:04:58 -0600

    day 9 part a

---
+++ 09/a.rb
@@ -0,0 +1,22 @@
+require "set"
+
+h = t = [0, 0]
+visited = Set.new([[0, 0]])
+dirs = { "U" => [0, 1], "D" => [0, -1], "R" => [1, 0], "L" => [-1, 0] }
+
+File.foreach("09/input.txt", chomp: true) do |line|
+  dir, dist = dirs[line[/[UDLR]/]], line[/\d+/].to_i
+
+  dist.times do
+    h = [h, dir].transpose.map(&:sum)
+    xdiff, ydiff = h[0] - t[0], h[1] - t[1]
+
+    unless xdiff.abs < 2 && ydiff.abs < 2 # unless adjacent
+      t = [t[0] + xdiff.clamp(-1, 1), t[1] + ydiff.clamp(-1, 1)]
+    end
+
+    visited << t
+  end
+end
+
+puts visited.count
