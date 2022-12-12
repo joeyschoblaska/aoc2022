2a8ca87b1062121452fe85692543fcfd593fd67c
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-12-12 07:36:16 -0600

    day 12 part b

--- 12/b.rb
+++ 12/b.rb
@@ -2,7 +2,7 @@ require "set"
 require_relative "../lib/grid"
 
 grid = Grid.from_file("12/input.txt")
-nodes = { grid.index("S") => { visited: false, dist: 0 } }
+nodes = { grid.index("E") => { visited: false, dist: 0 } }
 heights =
   { "S" => 0, "E" => 25 }.tap do |h|
     ("a".."z").each_with_index { |v, i| h[v] = i }
@@ -12,12 +12,12 @@ loop do
   cxy = nodes.select { |k, v| !v[:visited] }.min_by { |k, v| v[:dist] }[0]
   cv = grid[*cxy]
 
-  p nodes[cxy][:dist] and break if cv == "E"
+  p nodes[cxy][:dist] and break if cv == "a"
 
   grid.each_neighbor(*cxy, diags: false) do |nxy, nv|
     nodes[nxy] ||= { visited: false, dist: Float::INFINITY }
 
-    if heights[nv] - heights[cv] <= 1
+    if heights[cv] - heights[nv] <= 1
       nodes[nxy][:dist] = [nodes[nxy][:dist], nodes[cxy][:dist] + 1].min
     end
   end
